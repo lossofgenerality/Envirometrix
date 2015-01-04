2a4d1a1cff269f833c53321dccc86e223e5741d0
Jacob Simmons <jacobs10792@gmail.com>
2015-01-04 18:01:43 -0500

    Deleted superfluous files

--- deploy.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-svn update
-python manage.py migrate
-python manage.py collectstatic --noinput
-service celeryd-atmospherics restart
-service celerybeat-atmospherics restart
-service apache2 restart
-echo "Done"
